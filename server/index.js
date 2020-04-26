8a1fbb4511c4c66c00dbf394e5194e49e183e501
Josiah Roberts <josiah.david.roberts@gmail.com>
2020-04-26 17:45:01 +0800

    Try to fix CORS

--- server/index.js
+++ server/index.js
@@ -1,51 +1,54 @@
-const http = require('http')
-const port = 3456
-const { exec } = require('child_process');
-const fetch = require('node-fetch');
-const tmp = require('tmp-promise');
-const fs = require('fs').promises;
-const ExiftoolProcess = require('node-exiftool').ExiftoolProcess;
-const exiftoolBin = require('dist-exiftool');
+const http = require("http");
+const port = 3456;
+const { exec } = require("child_process");
+const fetch = require("node-fetch");
+const tmp = require("tmp-promise");
+const fs = require("fs").promises;
+const ExiftoolProcess = require("node-exiftool").ExiftoolProcess;
+const exiftoolBin = require("dist-exiftool");
 
 let cache = {
-    files: [],
-    lastLoad: 0,
-    loadingPromise: null,
-    lifetime: 240
-}
+  files: [],
+  lastLoad: 0,
+  loadingPromise: null,
+  lifetime: 240,
+};
 
 function parseExifDate(dateTime) {
   const b = dateTime.split(/\D/);
   return new Date(b[0], b[1] - 1, b[2], b[3], b[4], b[5]);
 }
 
-function getRawFiles () {
+function getRawFiles() {
   return new Promise((res, rej) => {
-    exec('gsutil ls gs://just-call-me-ryan/gallery/thumbs', (err, stdout, stderr) => {
-        if (err)
-            rej(err);
-        res(stdout)
-    })
+    exec(
+      "gsutil ls gs://just-call-me-ryan/gallery/thumbs",
+      (err, stdout, stderr) => {
+        if (err) rej(err);
+        res(stdout);
+      }
+    );
   });
 }
 
 async function loadFilenamesFromGcloud() {
   let raw = await getRawFiles();
-  return raw.split("\n")
-            .map(ln => ln.trim())
-            .filter(ln => ln.startsWith("gs://"))
-            .filter(ln => ln.endsWith(".jpg"))
-            .map(ln => ln.replace("gs://", "https://storage.googleapis.com/"))
-            .map(thumb => ({thumb, full: thumb.replace("thumbs/", "full/")}));
+  return raw
+    .split("\n")
+    .map((ln) => ln.trim())
+    .filter((ln) => ln.startsWith("gs://"))
+    .filter((ln) => ln.endsWith(".jpg"))
+    .map((ln) => ln.replace("gs://", "https://storage.googleapis.com/"))
+    .map((thumb) => ({ thumb, full: thumb.replace("thumbs/", "full/") }));
 }
 
 function loadFileData(files) {
-  const loadToTmpFile = async names => {
-      let response = await fetch(names.thumb);
-      let [binary, tf] = await Promise.all([response.buffer(), tmp.file()]);
-      await fs.writeFile(tf.path, binary);
-      return {...names, tmp: tf};
-  }
+  const loadToTmpFile = async (names) => {
+    let response = await fetch(names.thumb);
+    let [binary, tf] = await Promise.all([response.buffer(), tmp.file()]);
+    await fs.writeFile(tf.path, binary);
+    return { ...names, tmp: tf };
+  };
   return Promise.all(files.map(loadToTmpFile));
 }
 
@@ -53,18 +56,21 @@ async function loadMetadata(files) {
   let reader = new ExiftoolProcess(exiftoolBin);
 
   try {
-    await reader.open().then(pid => console.log('Started exiftool process %s', pid));
+    await reader
+      .open()
+      .then((pid) => console.log("Started exiftool process %s", pid));
 
     for (let file of files) {
-      file.metadata = (await reader.readMetadata(file.tmp.path, ['-File:all']))
-        .data[0];
+      file.metadata = (
+        await reader.readMetadata(file.tmp.path, ["-File:all"])
+      ).data[0];
     }
 
-    return files.map(file => ({
-      thumb: file.thumb, 
-      full: file.full, 
-      rating: file.metadata.Rating, 
-      date: parseExifDate(file.metadata.DateTimeOriginal)
+    return files.map((file) => ({
+      thumb: file.thumb,
+      full: file.full,
+      rating: file.metadata.Rating,
+      date: parseExifDate(file.metadata.DateTimeOriginal),
     }));
   } finally {
     await reader.close();
@@ -77,7 +83,7 @@ async function loadFilesWithMetadata() {
   try {
     return await loadMetadata(populatedFiles);
   } finally {
-    await Promise.all(populatedFiles.map(x => x.tmp.cleanup()));
+    await Promise.all(populatedFiles.map((x) => x.tmp.cleanup()));
   }
 }
 
@@ -90,29 +96,34 @@ async function loadAndCache() {
 }
 
 function loadFiles() {
-    if ((new Date() / 1000) - (cache.lastLoad / 1000) > cache.lifetime && !cache.loadingPromise) {
-        console.log("Loading files...");
-        cache.loadingPromise = loadAndCache();
-    }
-    
-    if (cache.lastLoad != 0)
-    {
-      console.log("Returning cache")
-      return cache.files;
-    }
+  if (
+    new Date() / 1000 - cache.lastLoad / 1000 > cache.lifetime &&
+    !cache.loadingPromise
+  ) {
+    console.log("Loading files...");
+    cache.loadingPromise = loadAndCache();
+  }
 
-    return cache.loadingPromise;
+  if (cache.lastLoad != 0) {
+    console.log("Returning cache");
+    return cache.files;
+  }
+
+  return cache.loadingPromise;
 }
 
 const requestHandler = async (request, response) => {
   console.log(`Incoming ${request.method} to ${request.url}`);
   if (request.url == "/images") {
-    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
     if (request.method == "OPTIONS") {
-      response.setHeader('Access-Control-Allow-Headers', 'pragma,cache-control');      
-      response.setHeader('Access-Control-Request-Method', '*');
-      response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+      response.setHeader(
+        "Access-Control-Allow-Headers",
+        "pragma,cache-control"
+      );
+      response.setHeader("Access-Control-Request-Method", "*");
+      response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
       response.writeHead(200);
       response.end();
       return;
@@ -123,14 +134,14 @@ const requestHandler = async (request, response) => {
   } else {
     response.end();
   }
-}
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log("something bad happened", err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
